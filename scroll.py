eac81da67e4e59fc8e30357f4891852a74eeb3db
Normal-case <8282chan94@gmail.com>
2021-08-10 16:34:09 +0900

    developing infinite scroll

--- scroll.py
+++ scroll.py
@@ -1,22 +1,32 @@
 import time
+import datetime
+import pdb
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
 
 def scroll_down(driver):
-    last_height = driver.execute_script("return document.body.scrollHeight")
+    SCROLL_PAUSE_SEC = 3
+    scroll_ = 0
 
     while True:
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        time.sleep(10)
+        
+        ret = driver.page_source
+        soup = BeautifulSoup(ret, 'lxml')
+        images = soup.find_all('img', attrs={'class': 'oCCRx'})
+        print(len(images))
+
+        for img in images:
+            try:
+                print(img['alt'])
+            except:
+                print('no alt')
+
+        print('='*100)
+        # scroll down
+        driver.execute_script(f'window.scrollTo({scroll_}, {scroll_} + 3000);')
+        scroll_ += 3000
+        time.sleep(SCROLL_PAUSE_SEC)
         new_height = driver.execute_script('return document.body.scrollHeight')
 
-        if new_height == last_height:
-            time.sleep(10)
-            new_height = driver.execute_script('return document.body.scrollHeight')
-
-            try:
-                driver.find_element_by_class_name("mye4qd").click()
-            except:
-
-                if new_height == last_height:
-                    break
-    
-    last_height = new_height
+        if new_height < scroll_:
+            break
